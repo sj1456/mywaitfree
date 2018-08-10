6d5dea4473c1ca31af2abeb9b5679383d6f93d99
Sai Jiang <tsjs2011@gmail.com>
2018-08-10 11:06:21 -0700

    Add a result button to the homepage to help show the current list directly

--- mywaitfree/views.py
+++ mywaitfree/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Guest
 from django.template import loader
